75866018f1d1a12c548c7c5348ed98bc52384cdc
Tom Burris <unsaneity@gmail.com>
2022-01-03 23:29:38 -0800

    fix compile-time errors

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,34 @@ extern crate websocket;
 pub mod algae {
     use std::{time};
     use std::time::Instant;
-    use websocket::receiver::Reader;
     use websocket::server::WsServer;
     use websocket::server::NoTlsAcceptor;
     use std::net::{TcpListener, TcpStream};
-    use websocket::sync::{Server as SyncServer, Writer};
+    use websocket::sync::{Server as SyncServer};
     use websocket::OwnedMessage;
-
+    use std::net::SocketAddr;
+    use websocket::sync::Client as WsClient;
 
     struct Player {
 
     }
     struct Client {
-        ws_client: websocket::sync::Client<websocket::stream::sync::TcpStream>,
-        ip: std::net::SocketAddr,
+        ws_client: WsClient<TcpStream>,
+        ip: SocketAddr,
+        should_close: bool
         // receiver: Reader<TcpStream>,
         // sender: Writer<TcpStream>
         // player: Player
+    }
+
+    impl Client {
+        pub fn new(ws_client: WsClient<TcpStream>, ip: SocketAddr) -> Client {
+            Client {
+                ws_client,
+                ip,
+                should_close: false
+            }
+        }
     }
 
     pub struct Server {
@@ -44,7 +55,7 @@ pub mod algae {
             }
             let frame_start = time::Instant::now();
             self.clients.append(&mut Server::accept_new_connections(&mut self.ws_server));
-            &mut self.handle_client_messages();
+            Server::handle_client_messages(&mut self.clients);
             // todo!("game logic");
             // todo!("send updates to clients");
             let tick_duration = frame_start.elapsed();
@@ -59,18 +70,19 @@ pub mod algae {
                 let ip = ws_client.peer_addr().unwrap();
                 println!("Connection from ip: {}", ip);
 
-                let (receiver, sender) = ws_client.split().unwrap();
+                // let (receiver, sender) = ws_client.split().unwrap();
 
 
-                clients.push(Client {ws_client, ip});
+                clients.push(Client::new(ws_client, ip));
             }
             clients
         }
-        fn handle_client_messages(&mut self) {
-            for client in self.clients {
-                for result in client.ws_client.incoming_messages() {
+        fn handle_client_messages(clients: &mut Vec<Client>) {
+            for client in clients {
+                'outer: for result in client.ws_client.incoming_messages() {
                     match result {
                         Ok(message) => {
+                            println!("debug a");
                             match message {
                                 OwnedMessage::Binary(data) => {
                                     match data[0] {
@@ -83,15 +95,15 @@ pub mod algae {
                                     }
                                 },
                                 _ => {
-                                    println!("received unknown message: {:?}, closing connection", message);
-                                    let _ = client.ws_client.shutdown();
+                                    println!("received unknown message: {:?}", message);
+                                    client.should_close = true;
                                 }
                             }
                         }
                         Err(error) => {
                             match error {
                                 websocket::WebSocketError::NoDataAvailable => {
-                                break;
+                                break 'outer;
                                 },
                                 _ => {
                                     println!("read websocket message error: {}", error);
