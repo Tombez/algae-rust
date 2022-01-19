be4923b06c9ad856f2ea1a45d4cbb4a8b5e33f56
Tom Burris <unsaneity@gmail.com>
2022-01-19 08:33:25 -0800

    cell creation on player start

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@ mod vec2;
 mod protocol;
 
 pub mod algae {
+    const SERVER_PORT: u16 = 8080;
+    const CELL_START_R: f32 = 5.0;
+
+    use std::rc::Rc;
     use std::{time};
     use std::time::Instant;
     use simple_websockets::{Event, EventHub, Responder, Message};
@@ -21,15 +25,23 @@ pub mod algae {
             _ => ""
         }
     }
+
+    struct Cell {
+        id: u32,
+        pos: Vec2<f32>,
+        r: f32,
+    }
+
     struct Player {
         name: String,
-        mouse: Vec2<f32>
+        mouse: Vec2<f32>,
+        cell_ids: Vec<u32>
     }
+
     struct Client {
         ws_client: Responder,
         player: Option<Player>
     }
-
     impl Client {
         pub fn new(ws_client: Responder) -> Client {
             Client { ws_client, player: None }
@@ -38,9 +50,10 @@ pub mod algae {
 
     pub struct Server {
         ws_server: EventHub,
-        clients: HashMap<u64, Client>,
+        clients: HashMap<u64, Box<Client>>,
         last_tick_instant: Instant,
-        last_id: u32
+        last_id: u32,
+        cells_by_id: HashMap<u32, Box<Cell>>,
     }
     impl Default for Server {
         fn default() -> Server {
@@ -49,19 +62,22 @@ pub mod algae {
     }
     impl Server {
         pub fn new() -> Server {
-            let ws_server = simple_websockets::launch(8080)
-            .expect("failed to listen on port 8080");
+            let failed_str = std::format!("failed to listen on port {}", SERVER_PORT);
+            let ws_server = simple_websockets::launch(SERVER_PORT)
+            .expect(&failed_str);
+            let clients = HashMap::new();
             let last_tick_instant = time::Instant::now();
-            Server {ws_server, clients: HashMap::new(), last_tick_instant, last_id: 0}
+            let cells_by_id = HashMap::new();
+            Server {ws_server, clients, last_tick_instant, last_id: 0, cells_by_id}
         }
-        pub fn start(&mut self) {
-            loop { self.game_loop(); }
+        pub fn start(mut self) {
+            loop { self = self.game_loop(); }
         }
-        fn game_loop(&mut self) {
+        fn game_loop(mut self) -> Self {
             let inter_tick_duration = self.last_tick_instant.elapsed();
             if inter_tick_duration < std::time::Duration::new(0, 1_000_000_000 / 60) {
                 //println!("spinning because duration is not large enough: {:?}, min: {:?}", inter_tick_duration, std::time::Duration::new(0, 1_000_000_000 / 60));
-                return;
+                return self;
             }
             let frame_start = time::Instant::now();
 
@@ -75,7 +91,7 @@ pub mod algae {
                                 let client = Client::new(responder);
                                 let msg = vec![2u8, 60u8, 200u8];
                                 client.ws_client.send(Message::Binary(msg));
-                                self.clients.insert(client_id, client);
+                                self.clients.insert(client_id, Box::new(client));
                             },
                             Event::Disconnect(client_id) => {
                                 println!("Client #{} disconnected.", client_id);
@@ -83,8 +99,8 @@ pub mod algae {
                             },
                             Event::Message(client_id, message) => {
                                 // println!("Received a message from client #{}: {:?}", client_id, message);
-                                let client = self.clients.get_mut(&client_id).unwrap();
                                 if let Message::Binary(bytes) = message {
+                                    let mut client = self.clients.remove(&client_id).unwrap();
                                     let opcode = ClientOpcode::from(bytes[0]);
                                     match opcode {
                                         ClientOpcode::VersionHandshake => {
@@ -97,7 +113,15 @@ pub mod algae {
                                             let name = string_at(&bytes, 1);
                                             println!("play msg name: {}", name);
                                             let mouse = Vec2 {x: 0f32, y: 0f32};
-                                            let player = Player {name: name.to_string(), mouse};
+                                            let cell_ids = vec![];
+                                            let mut player = Player {name: name.to_string(), mouse, cell_ids};
+
+                                            self.last_id += 1;
+                                            let id = self.last_id;
+                                            let pos = Vec2{x: 0f32, y: 0f32};
+                                            let cell = Cell{id, pos, r: CELL_START_R};
+                                            player.cell_ids.push(cell.id);
+
                                             client.player = Some(player);
                                         }
                                         ClientOpcode::MousePos => {
@@ -107,6 +131,7 @@ pub mod algae {
                                             println!("unknown opcode: {:?}, msg: {:?}", opcode, bytes);
                                         }
                                     }
+                                    self.clients.insert(client_id, client);
                                 }
                             },
                         }
@@ -119,7 +144,12 @@ pub mod algae {
             let _tick_duration = frame_start.elapsed();
             // println!("finished tick with duration: {:?}", tick_duration);
             self.last_tick_instant = frame_start;
+            self
         }
+        // fn spawnPlayer(mut self, client: &mut Client, name: &str) -> Self {
+
+        //     self
+        // }
     }
 }
 
--- src/protocol.rs
+++ src/protocol.rs
@@ -2,19 +2,18 @@
 #[repr(u8)]
 pub enum ServerOpcode {
     Update = 16,
-
 }
 #[derive(PartialEq, PartialOrd, Debug)]
 #[repr(u8)]
 pub enum ClientOpcode {
     Play = 0,
     Spectate = 1,
-    Unknown = 2,
     MousePos = 16,
     Split = 17,
     QKeyDown = 18,
     QKeyUp = 19,
     Eject = 21,
+    Unknown,
     VersionHandshake = 254,
     Handshake2 = 255,
 }
@@ -23,6 +22,13 @@ impl From<u8> for ClientOpcode {
         match v {
             0 => ClientOpcode::Play,
             1 => ClientOpcode::Spectate,
+            16 => ClientOpcode::MousePos,
+            17 => ClientOpcode::Split,
+            18 => ClientOpcode::QKeyDown,
+            19 => ClientOpcode::QKeyUp,
+            21 => ClientOpcode::Eject,
+            254 => ClientOpcode::VersionHandshake,
+            255 => ClientOpcode::Handshake2,
             _ => ClientOpcode::Unknown,
         }
     }
